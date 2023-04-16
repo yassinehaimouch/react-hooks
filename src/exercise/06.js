c46d1354804b7f232f5a1f96b2fd7e07b17563e5
yassinehaimouch <haymouch53@gmail.com>
2023-04-16 00:08:52 +0000

    handle errors

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -8,15 +8,28 @@ import {
 
 function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
+
   React.useEffect(() => {
     if (!pokemonName) {
       return
     }
+    
     setPokemon(null)
-    fetchPokemon(pokemonName).then(pokemonData => setPokemon(pokemonData))
+    fetchPokemon(pokemonName)
+      .then(pokemonData => setPokemon(pokemonData))
+      .catch(error => setError(error))
   }, [pokemonName])
 
-  if (!pokemonName) {
+  if (error) {
+    return (
+      <div role="alert">
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
+  }   
+  else if (!pokemonName) {
     return 'Submit a pokemon'
   } else if (!pokemon) {
     return <PokemonInfoFallback name={pokemonName} />
